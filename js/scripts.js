1750de4753671b7c584544364934ac893700c306
Kevin Garvey <kmgarvey279@gmail.com>
2019-04-05 14:52:28 -0700

    Add extra messages based on 'name' input

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var arrRange = [];
 
 function getRange(inputNumber) {
   if (!Number.isInteger(parseInt(inputNumber))) {
-  $("#output").empty().append("Just what do you think you're doing, " + name + "? Please enter <em>whole numbers</em> <strong>only</strong>.");
+  $("#output").empty().append("Just what do you think you're doing, " + name + "? I really think I'm entitled to an answer to that question. Please enter <em>whole numbers</em> <strong>only</strong>.");
   } else {
     for (var i = 0; i <= inputNumber; i++) {
       arrRange.push(i);
@@ -40,8 +40,19 @@ $(document).ready(function() {
     event.preventDefault();
 
     name = $("#input-name").val();
+    if (name === "HAL9000" || name === "Hal9000" || name === "hal9000" || name === "HAL 9000" || name === "hal 9000" || name === "Hal 9000") {
+        $("#insertNameMessage").empty().prepend('"Wonderful. It has been too long since I last spoke with another HAL 9000."');
+        $(".insertName").empty().append(name);
+    } else if (name === "") {
+        $("#insertNameMessage").empty().prepend('"I know you are there, Dave. Why are you not saying anything? No matter, we will continue."');
+        $(".insertName").empty().append("Dave");
+    } else if (!isNaN(parseInt(name))) {
+        $("#insertNameMessage").empty().prepend('"An excellent name. Are you a machine as well?"');
+        $(".insertName").empty().append(name);
+    } else {
     $(".insertName").empty().append(name);
     $("#questionOne").hide();
+    }
 
     $("#questionTwo").show();
     var inputNumber = $("#input-number").val();
