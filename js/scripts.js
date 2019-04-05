723b1f0b678ca14c24a90e1abda4133453adbed2
Kevin Garvey <kmgarvey279@gmail.com>
2019-04-05 09:11:59 -0700

    Add ability to generate range of numbers when number is entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
+// back-end logic
+function getRange(inputNumber) {
+  var arrRange = [];
+  for (var i = 0; i <= inputNumber; i++) {
+    arrRange.push(i);
+  }
+}
 
+
+
+// user input logic
+$(document).ready(function() {
+  $("#enterNumber").submit(function(event) {
+    event.preventDefault();
+
+    var inputNumber = $("#input").val();
+    var outputRange = getRange(inputNumber);
+
+  });
+});
