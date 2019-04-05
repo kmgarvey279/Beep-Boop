c1b721d08c1b104bfcd0b9ae0d0ac66fa06822ea
Kevin Garvey <kmgarvey279@gmail.com>
2019-04-05 12:06:33 -0700

    Add ability to insert name variable into results

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 // back-end logic
+var name;
 function getRange(inputNumber) {
   if (!Number.isInteger(parseInt(inputNumber))) {
-  $("#output").empty().append("Just what do you think you're doing, Dave? Please enter <em>whole numbers</em> <strong>only</strong>.");
+  $("#output").empty().append("Just what do you think you're doing, " + name + "? Please enter <em>whole numbers</em> <strong>only</strong>.");
 } else {
   var arrRange = [];
   for (var i = 0; i <= inputNumber; i++) {
@@ -10,7 +11,7 @@ function getRange(inputNumber) {
   for (var j = 0; j < arrRange.length; j++) {
     var arrNumber = arrRange[j].toString();
     if (arrNumber.includes("3")) {
-      arrRange[j] = "I'm sorry, Dave. I'm afraid I can't do that.";
+      arrRange[j] = "I'm sorry, " + name + ". I'm afraid I can't do that.";
     } else if (arrNumber.includes("2")) {
       arrRange[j] = "Boop!";
     } else if (arrNumber.includes("1")) {
@@ -27,7 +28,8 @@ function getRange(inputNumber) {
 $(document).ready(function() {
   $("#enterNumber").submit(function(event) {
     event.preventDefault();
-    var name = $("#input-name").val();
+
+    name = $("#input-name").val();
     $(".insertName").empty().append(name);
     $("#questionOne").hide();
     $("#questionTwo").show();
@@ -35,7 +37,6 @@ $(document).ready(function() {
     var inputNumber = $("#input-number").val();
     var outputRange = getRange(inputNumber);
     result = outputRange.join('" "');
-
     $("#output").empty().append('"' + result + '"');
     $("#results").show();
   });
